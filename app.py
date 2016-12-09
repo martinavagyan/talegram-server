1ef83da4e85d4d3b0ab82fbee994c15005fbf17d
martinavagyan <martinavagyan@gmail.com>
2016-12-09 11:13:04 +0100

    updated sotries

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request, send_file
 app = Flask(__name__)
 
 from flask import render_template
@@ -6,6 +6,10 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/getstory') # this is a job for GET, not POST
+def plot_csv():
+    return app.send_static_file('docs/AmazingStory/cover.png')
 
 
 if __name__ == "__main__":
